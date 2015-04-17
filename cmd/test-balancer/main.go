33aa200cd8384432a11870c21815f947f1ac7c28
Daniel, Dao Quang Minh <dqminh89@gmail.com>
2015-04-17 04:17:09 -0700

    verify round-robin load balancing
    Signed-off-by: Daniel, Dao Quang Minh <dqminh89@gmail.com>

---
+++ cmd/test-balancer/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var (
+	endpoint = "http://172.17.8.102/"
+	interval = 10 * time.Millisecond
+	duration = 5 * time.Second
+	workers  = 5
+)
+
+func main() {
+	responses := make(chan *http.Response)
+	ticker := time.NewTicker(interval)
+
+	for i := 0; i < workers; i++ {
+		go hit(endpoint, ticker.C, responses)
+	}
+
+	hits := int(duration / interval)
+	results := make([]*http.Response, 0, hits)
+	after := time.After(duration)
+	for hits > 0 {
+		select {
+		case <-after:
+			ticker.Stop()
+			hits = 0
+		case r := <-responses:
+			results = append(results, r)
+			hits--
+		}
+	}
+
+	goapp := make(map[string]int)
+	nginx := make(map[string]int)
+
+	fmt.Printf("got %d responses\n", len(results))
+	for _, r := range results {
+		goName := r.Header.Get("X-Goserved-By")
+		nginxName := r.Header.Get("X-Served-By")
+		goapp[goName]++
+		nginx[nginxName]++
+	}
+	fmt.Println(goapp)
+	fmt.Println(nginx)
+}
+
+func hit(endpoint string, tick <-chan time.Time, responses chan<- *http.Response) {
+	for _ = range tick {
+		r, err := http.Get(endpoint)
+		if err == nil {
+			responses <- r
+		}
+	}
+}
